571423552a33c534475ac7be17264fa65941aede
Diego Sandoval <sandovdiego@gmail.com>
2023-02-28 19:22:06 -0300

    add helix editor install script

---
+++ other/helix_install.sh
@@ -0,0 +1,9 @@
+curl -L https://github.com/helix-editor/helix/releases/download/22.12/helix-22.12-x86_64.AppImage -O
+
+chmod +x helix*.AppImage
+
+sudo mv helix*.AppImage /usr/local/bin/helix
+
+sudo ln -s /usr/local/bin/helix /usr/local/bin/hx
+
+
