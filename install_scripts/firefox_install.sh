bc00dc7e4d66cb12c31be3196e17e9da2948c310
Diego Sandoval <sandovdiego@gmail.com>
2023-04-10 18:36:04 -0400

    improve firefox install script
    - install in /opt
    - create a symbolic link in /usr/local/bin/firefox to ff binary

--- install_scripts/firefox_install.sh
+++ install_scripts/firefox_install.sh
@@ -1,15 +1,11 @@
 wget -O firefox.tar.bz2 'https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US';
-
 tar xvf firefox.tar.bz2
-
 rm firefox.tar.bz2
 
-mkdir -p ~/Documents/apps/bin/
-
-mv firefox/ ~/Documents/apps/bin/
+sudo mv firefox/ /opt/firefox
 
 rm ~/.local/share/applications/*firefox*
+cp res/firefox.desktop ~/.local/share/applications/
 
-cp ./firefox.desktop ~/.local/share/applications/
-
+sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
 
