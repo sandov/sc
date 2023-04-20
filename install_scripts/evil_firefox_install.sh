4d36fe069e2d772de5c97ceb7426ae2e041089d0
Diego Sandoval <sandovdiego@gmail.com>
2023-04-20 00:51:15 -0400

    add evil firefox installation script

---
+++ install_scripts/evil_firefox_install.sh
@@ -0,0 +1,18 @@
+wget -O firefox.tar.bz2 'https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US';
+tar xvf firefox.tar.bz2
+rm firefox.tar.bz2
+
+mv firefox/ evil-firefox
+
+sudo rm -rf /opt/evil-firefox
+sudo mv evil-firefox/ /opt/.
+
+rm ~/.local/share/applications/evil-firefox.desktop
+cp res/evil-firefox.desktop ~/.local/share/applications/
+
+sudo mkdir -p /usr/local/bin
+sudo rm /usr/local/bin/evil-firefox
+sudo ln -s /opt/evil-firefox/firefox /usr/local/bin/evil-firefox
+
+sudo convert /opt/evil-firefox/browser/chrome/icons/default/default128.png -channel RGB -negate /opt/evil-firefox/browser/chrome/icons/default/default128.png
+
