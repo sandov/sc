c899a25b7d75e9e8e4cda978af7d4ddc7a340c55
Diego Sandoval <sandovdiego@gmail.com>
2023-08-18 15:07:15 -0400

    improve io_scripts for 14s

--- io_scripts/aux/14s_speakers_toggle_setup.sh
+++ io_scripts/aux/14s_speakers_toggle_setup.sh
@@ -1,5 +1,6 @@
+# RUN AS SUDO
+
 apt install -y alsa-tools;
 
 echo "ALL ALL=NOPASSWD: /usr/bin/hda-verb" >> /etc/sudoers
-
 
---
+++ io_scripts/aux/14s_touchpad_env_var_setup.sh
@@ -0,0 +1,3 @@
+echo export MOZ_USE_XINPUT2=1 | sudo tee /etc/profile.d/use-xinput2.sh
+
+
