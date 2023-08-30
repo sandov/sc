33297c65ac002bbd8f9e79a29e69d9e3a61c3da4
Diego Sandoval <sandovdiego@gmail.com>
2023-08-30 00:19:52 -0400

    fix joplin install

--- install_scripts/joplin_install.sh
+++ install_scripts/joplin_install.sh
@@ -1,11 +1,11 @@
-#curl -L https://objects.joplinusercontent.com/v2.11.11/Joplin-2.11.11.AppImage -O
+curl -L https://objects.joplinusercontent.com/v2.11.11/Joplin-2.11.11.AppImage -O
 
-#mv Joplin*.AppImage Joplin.AppImage
+mv Joplin*.AppImage Joplin.AppImage
 
-#chmod +x Joplin.AppImage
+chmod +x Joplin.AppImage
 
-#sudo mkdir -p /opt/joplin/
-#sudo mv ./Joplin.AppImage /opt/joplin/
+sudo mkdir -p /opt/joplin/
+sudo mv ./Joplin.AppImage /opt/joplin/
 
 cp res/joplin.desktop ~/.local/share/applications/
 
