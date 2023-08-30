939f59db39a8c455cf7f16bbc2502c1b924a2bf6
Diego Sandoval <diegosandovalburgos@gmail.com>
2023-08-30 00:08:24 -0400

    add Joplin install script and desktop file

---
+++ install_scripts/joplin_install.sh
@@ -0,0 +1,11 @@
+#curl -L https://objects.joplinusercontent.com/v2.11.11/Joplin-2.11.11.AppImage -O
+
+#mv Joplin*.AppImage Joplin.AppImage
+
+#chmod +x Joplin.AppImage
+
+#sudo mkdir -p /opt/joplin/
+#sudo mv ./Joplin.AppImage /opt/joplin/
+
+cp res/joplin.desktop ~/.local/share/applications/
+
