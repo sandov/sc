c29f589e579a92f04b14f96fb1c284b7fb450843
Diego Sandoval <16181660+sandov@users.noreply.github.com>
2024-05-31 21:11:28 -0400

    update golang version

--- install_scripts/golang_install.sh
+++ install_scripts/golang_install.sh
@@ -1,7 +1,7 @@
-curl -L https://go.dev/dl/go1.21.6.linux-amd64.tar.gz -O
+curl -L https://go.dev/dl/go1.22.3.linux-amd64.tar.gz -O
 
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.21.6.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
 
-rm ./go1.21.6.linux-amd64.tar.gz
+rm ./go1.22.3.linux-amd64.tar.gz
 
 echo "export PATH=$PATH:/usr/local/go/bin" > ~/.profile
