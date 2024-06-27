2a12e2a2ccd71ee9f2732726853666fdbc3f0007
Diego Sandoval <16181660+sandov@users.noreply.github.com>
2024-06-26 21:37:35 -0400

    update go version, improve script

--- install_scripts/golang_install.sh
+++ install_scripts/golang_install.sh
@@ -1,8 +1,10 @@
-curl -L https://go.dev/dl/go1.22.3.linux-amd64.tar.gz -O
+dl_url="https://go.dev/dl/go1.22.4.linux-amd64.tar.gz"
 
-sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
+curl -L $dl_url -O
 
-rm ./go1.22.3.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.*.linux-amd64.tar.gz
+
+rm ./go1.*.linux-amd64.tar.gz
 
 echo "export GOPATH=\$HOME/.go" >> ~/.profile
 echo "export PATH=\$PATH:/usr/local/go/bin:\$GOPATH/bin" >> ~/.profile
